dbca59048d716418063bd26d56f88c7b91bdff42
mubashir malik <mubashir.hussain@ooober.com>
2022-01-05 12:56:32 +0500

    changes

--- src/axios.js
+++ src/axios.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'http://test1.foilsurfcamps.com/api/';
+axios.defaults.baseURL = 'https://videocall.esystemsco.com/public/api/';
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
